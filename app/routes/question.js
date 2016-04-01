389a9d9906693338eee7a35ff2eac61f2dd58860
alex <alex.m.larson@gmail.com>
2016-04-01 13:09:59 -0700

    fixed but

--- app/routes/question.js
+++ app/routes/question.js
@@ -2,11 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params){
-    var data = this.store.findRecord('question', params.question_id);
+    return this.store.findRecord('question', params.question_id);
   },
+  
   actions: {
     saveAnswer(params) {
       var newAnswer = this.store.createRecord('answer', params);
+        // console.log(this.store.findRecord('question', params.question_id));
+
       var question = params.question;
       question.get('answers').addObject(newAnswer);
       newAnswer.save().then(function () {
