f281829f3dded9aa7dbfd64a233e7c96bc6e2de9
alex <alex.m.larson@gmail.com>
2016-04-01 10:13:11 -0700

    add answer save functionality

--- app/routes/question.js
+++ app/routes/question.js
@@ -2,9 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params){
-    return this.store.findRecord('question', params.question_id)
+    return this.store.findRecord('question', params.question_id);
   },
   actions: {
-    
+    saveAnswer(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      newAnswer.save();
+    }
   }
 });
